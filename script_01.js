1fde53ff7248c0681c51163b569e8d94204b7743
Mertha89 <mertha@live.de>
2023-05-17 10:37:12 +0200

    zwischenablage

--- script_01.js
+++ script_01.js
@@ -31,10 +31,77 @@
 
 
 /***** Objekte 1 Daten/Funktionen *******/
+let date = new Date();
+let hour = date.getHours();
+let person = {
+                firstName: "Peter",
+                familyName: "Maffay",
+                salery:[120000, 160000],
+                permission: true,
+                sayHello: 
+                        function (params) {
+                            if (hour >= 5 && hour < 12) {
+                                console.log("Guten Morgen " + this.firstName + "!");
+                              } else if (hour >= 12 && hour < 18) {
+                                console.log("Hallo " + this.firstName + "!");
+                              } else {
+                                console.log("Tschüss " + this.firstName + "!");
+                              }
+                              return date;
+                        }
 
 
 
 
+
+};
+
+
+// output(person);
+// output(person.firstName)
+// output(person.familyName)
+// output(person.salery[1])
+// output(person.permission)
+
+// const text = "Ich bin " + person.firstName +
+//                 " und verdiene " + person.salery[1] +
+//                 " p.a";
+// output(text);
+// output(person.sayHello());
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//     fullName: function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
+
+//   output(person);
+ 
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+// nested object {{{}}}
+
+let baikal = {
+                value:"10m",
+                deep:{
+
+                        deeper:{
+                                    deepst:"Das Licht - auf 1642m",
+                                    temperatur:"4 Grad Celsius"
+
+                        }
+                    }
+
+
+
+            };
+
+output(baikal);
+output(baikal.value)
 
 
 
